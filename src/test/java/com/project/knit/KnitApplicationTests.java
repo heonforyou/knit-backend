df9bc686dedb921c363781dd580d91965bfc1016
key6020 <keysujin92@gmail.com>
2021-02-02 02:25:31 +0900

    Initial Commit

---
+++ src/test/java/com/project/knit/KnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.knit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
